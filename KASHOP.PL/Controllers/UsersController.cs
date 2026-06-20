fb36f2f6e6df7b1236d82e9f9dc15b78a5bdd18e
nuwar1 <nuwarkurdi015@gmail.com>
2026-06-20 17:50:05 +0300

    set up project structure

---
+++ KASHOP.PL/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using KASHOP.DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace KASHOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        ApplicationDbContext _context;
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
